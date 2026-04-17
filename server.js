4feef3f25a4c5f5f184da97d094683f3a005f2e5
MidhunaMohanraj <97339582+MidhunaMohanraj@users.noreply.github.com>
2026-04-17 16:03:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+   
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.static(path.join(__dirname, "public")));
